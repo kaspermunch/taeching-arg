488f8478f8f4d98551183f0c6c20932bc3352392
Kasper Munch <kmt@candace.lan>
2020-02-15 23:27:07 +0100

    Completed functionality

--- teachingarg/__init__.py
+++ teachingarg/__init__.py
@@ -1,32 +1,4 @@
 
- 
-"""
-# Poppen Dashboard
-
-Make ARG
-
-ARG class:
-- All nodes in a list
-- Each node has attributes: id, x, y, ancestral_intervals
-- Copy interval functions from genominterv to manage intervals and add function to split a set of intervals at a break point.
-- leaves of class Leaf
-- Coalescence nodes of class Coalescence
-	- Attributes: id, x, y, ancestral_intervals, children.
-- Recombination nodes of class Recombination.
-	- Attributes is, x, y, break_point, child.
-	- Upon recombination, duplicate node and split ancestral intervals. 
-
-What class should nodes created by recombination be?
-
-Generate ARG
-
-Do constrained spring layout and populate x and y attributes of each node.
-
-Get tree sequence bottom up. Start with one tree and add coalescence events 
-Upon recombination duplicate tree that has the breakpoint
-
-
-"""
 from math import exp, log
 import networkx.algorithms.non_randomness
 
@@ -36,110 +8,81 @@ random.seed(3)
 
 from random import random, shuffle, choices
 from functools import partial
+from copy import deepcopy
 import networkx as nx
 from functools import reduce
-
 from numpy.random import exponential
-
 import numpy as np
-
 from time import sleep
+import matplotlib.pyplot as plt
 
-
-# Lineages are the edges of our graph
 
 class Lineage(object):
-
-    def __init__(self, node, intervals):
-        self.node = node
+    """
+    Lineages are the edges of our graph
+    """
+    def __init__(self, down, intervals):
+        self.down = down # node at bottom of edte
+        self.up = None # node at top of edge
         self.intervals = intervals
 
     def __repr__(self):
-        return f'{self.node}:{self.intervals}'
+        return f'{self.down}:{self.intervals}'
 
-# Leaf, Coalscent and Recombination are the nodes of the graph
+class Node():
+    """
+    Leaf, Coalscent and Recombination are the nodes of the graph
+    """
+    def __hash__(self):
+        return self.nodeid
 
-class Leaf(object):
+    def __eq__(self, other):
+        return self.nodeid == other.nodeid  
+
+    def __repr__(self):
+        return f'{self.nodeid}'
+
+class Leaf(Node):
 
     def __init__(self, nodeid, height):
         self.nodeid = nodeid
         self.height = height
         self.intervals = [(0, 1)]
-        # self.parents = []
         self.parent = None
         self.xpos = None
 
-    def __repr__(self):
-        return f'{self.nodeid}'
-
-class Coalescent(object):
+class Coalescent(Node):
 
     def __init__(self, nodeid, height, children):
         self.nodeid = nodeid
         self.height = height
         self.children = children
-        # self.parents = []
         self.parent = None
         self.xpos = None
 
-    def __repr__(self):
-        return f'{self.nodeid}'
-
-class Recombination(object):
+class Recombination(Node):
 
     def __init__(self, nodeid, height, child, recomb_point):
         self.nodeid = nodeid
         self.height = height
         self.child = child
-        # self.parents = []        
         self.left_parent = None
         self.right_parent = None
         self.recomb_point = recomb_point
         self.xpos = None
 
-    def __repr__(self):
-        return f'{self.nodeid}'
-
-
-# In all of the following, the list of intervals must be sorted and 
-# non-overlapping. We also assume that the intervals are half-open, so
-# that x is in tp(start, end) iff start <= x and x < end.
-
 def flatten(list_of_tps):
-    """
-    Convert a list of sorted intervals to a list of endpoints.
-    :param query: Sorted list of (start, end) tuples.
-    :type query: list
-    :returns: A list of interval ends
-    :rtype: list
-    """
+ 
     return reduce(lambda ls, ival: ls + list(ival), list_of_tps, [])
 
 
 def unflatten(list_of_endpoints):
-    """
-    Convert a list of sorted endpoints into a list of intervals.
-    :param query: Sorted list of ends.
-    :type query: list
-    :returns: A list of intervals.
-    :rtype: list
-    """
+
     return [ [list_of_endpoints[i], list_of_endpoints[i + 1]]
           for i in range(0, len(list_of_endpoints) - 1, 2)]
 
-
 def merge(query, annot, op):
-    """
-    Merge two lists of sorted intervals according to the boolean function op.
-    :param query: List of (start, end) tuples.
-    :type query: list
-    :param query: List of (start, end) tuples.
-    :type query: list
-    :param op: Boolean function taking two a bolean arguments.
-    :type op: function
-    :returns: A list of interval merged according to op
-    :rtype: list
-    """
+
     a_endpoints = flatten(query)
     b_endpoints = flatten(annot)
 
@@ -202,8 +145,42 @@ def interval_split(intervals, pos):
             right.append((s, e))
     return left, right
 
-def get_arg_nodes(n=5, N=10000, r=1e-8, L=2e3):
+def x_positions_traverse(node, offset):
+    """
+    Recursive function for adding x positions
+    """
+    offset -= 1
+    if type(node) is Leaf:
+        return 1 # offset
+        # do nothing
+    if type(node) is Recombination:
+        # add xpos to children
+        node.child.down.xpos = node.xpos
+        offset = x_positions_traverse(node.child.down, offset)
+        # add xpos to node if both parents are positioned
+        if node.left_parent.up.xpos is not None and node.right_parent.up.xpos is not None:
+            left, right = sorted([node.left_parent.up.xpos, node.right_parent.up.xpos])
+            x = left + (right - left) / 2 
+            node.xpos = x
+    if type(node) is Coalescent:
+        # add xpos to children
+        node.children[0].down.xpos = node.xpos - offset
+        node.children[1].down.xpos = node.xpos + offset
+        for child in node.children:
+            x_positions_traverse(child.down, offset)
 
+def add_node_x_positions(nodes):
+    """
+    Adds x positions in place
+    """
+    offset = len(nodes)-1
+    nodes[-1].xpos = offset
+    x_positions_traverse(nodes[-1], offset)
+
+def get_arg_nodes(n=5, N=10000, r=1e-8, L=2e3):
+    """
+    Simulates an ARG
+    """
     # because we use the sequence interval from 0 to 1
     r = r * L
 
@@ -211,9 +188,9 @@ def get_arg_nodes(n=5, N=10000, r=1e-8, L=2e3):
     live = list() # live lineages
 
     for i in range(n):
-        n = Leaf(i, height=0)
-        nodes.append(n)
-        lin = Lineage(n, [(0, 1)])
+        leaf = Leaf(i, height=0)
+        nodes.append(leaf)
+        lin = Lineage(down=leaf, intervals=[(0, 1)])
         live.append(lin)
         last_node = i # max node number used so far
 
@@ -225,26 +202,24 @@ def get_arg_nodes(n=5, N=10000, r=1e-8, L=2e3):
         rec_prob = r * tot_ancestral
         wating_time = exponential(1 / (coal_prob + rec_prob))
         height = wating_time + nodes[-1].height
-        print(coal_prob, rec_prob, tot_ancestral)
         if random() < coal_prob / (coal_prob + rec_prob):
             # coalescence
             lin_a, lin_b = live.pop(), live.pop()
             intervals = interval_union(lin_a.intervals, lin_b.intervals)
-
             # new node
             node_c = Coalescent(nodeid=last_node+1, height=height, 
-                        children=[lin_a.node, lin_b.node])
+                        children=[lin_a, lin_b])
             last_node += 1
             nodes.append(node_c)
 
+            # add node to top of coalescing lineages
+            lin_a.up = node_c
+            lin_b.up = node_c
+
             # new lineage
-            lin_c = Lineage(node_c, intervals) # fixme intervals
+            lin_c = Lineage(down=node_c, intervals=intervals) # fixme intervals
             live.append(lin_c)
-
-            # add parent for children
-            lin_a.node.parent = node_c
-            lin_b.node.parent = node_c
-
+            node_c.parent = lin_c
         else:
             # recombination
             rec_lin = choices(live, weights=[interval_sum(x.intervals) for x in live], k=1)[0]
@@ -266,25 +241,26 @@ def get_arg_nodes(n=5, N=10000, r=1e-8, L=2e3):
 
             # recombination node
             rec_node = Recombination(nodeid=last_node+1, height=height, 
-                child=rec_lin.node, recomb_point=recomb_point)
+                child=rec_lin, recomb_point=recomb_point)
             last_node += 1
             nodes.append(rec_node)
 
             # two new lineages both refering back to recombination node
             intervals_a, intervals_b = interval_split(rec_lin.intervals, recomb_point)
             assert interval_sum(intervals_a) and interval_sum(intervals_b) 
-            lin_a = Lineage(rec_node, intervals_a)
-            lin_b = Lineage(rec_node, intervals_b)
-            print('r', intervals_a, intervals_b)
+            lin_a = Lineage(down=rec_node, intervals=intervals_a)
+            lin_b = Lineage(down=rec_node, intervals=intervals_b)
             live.append(lin_a)
             live.append(lin_b)
 
             # add parents of node
-            rec_node.left_parent = lin_a.node
-            rec_node.right_parent = lin_b.node
+            rec_node.left_parent = lin_a
+            rec_node.right_parent = lin_b
 
             # add parent for child
-            rec_lin.node.parent = rec_node
+            rec_lin.up = rec_node
+
+    add_node_x_positions(nodes)
 
     return nodes
 
@@ -301,42 +277,19 @@ def _rescale_layout(pos, scale=1):
         pos[:, i] *= scale / lim
     return pos
 
-def add_node_x_positions(nodes):
-
-    for _ in range(len(nodes)*10):
-        try:
-            nodes[-1].xpos = 0
-
-            for node in reversed(nodes):
-                if node.xpos is None:
-                    # if type(node.parents[0]) is Recombination:
-                    #     node.xpos = node.parents[0].xpos
-                    if type(node.parent) is Recombination:
-                        node.xpos = node.parent.xpos
-
-            for i, node in enumerate(reversed(nodes)):
-                if type(node) is Coalescent:
-                    left, right = node.children
-                    left.xpos = node.xpos - (len(nodes)-1-i)
-                    right.xpos = node.xpos + (len(nodes)-1-i)
-
-            for node in reversed(nodes):
-                if type(node) is Recombination:
-                    # left, right = sorted([n.xpos for n in node.parents])
-                    left, right = sorted([node.left_parent.xpos, node.right_parent.xpos])
-                    x = left + (right - left) / 2 
-                    node.xpos = x
-        except:
-            pass
-
 def get_positions(nodes):
-
+    """
+    Gets list of x,y positions for nodes
+    """
     positions = list()
     for node in nodes:
         positions.append((node.xpos, node.height))
     return positions
 
 def get_breakpoints(nodes):
+    """
+    Gets list of recombination break points
+    """    
     # get list of break points
     breakpoints = list()
     for n in nodes:
@@ -344,106 +297,133 @@ def get_breakpoints(nodes):
             breakpoints.append(n.recomb_point)
     return sorted(breakpoints)
 
-# def condition(node, child, leftbreak=None, rightbreak=None):
+def traverse_marginal(node, interval):
+    """
+    Recursive function for getting marginal tree/ARG
+    """    
+    node = deepcopy(node)
+    tree_nodes = set()
+    if type(node) is Leaf:
+        tree_nodes.add(node)
+    if type(node) is Recombination:
+        if interval_intersect([interval], node.child.intervals):
+            tree_nodes.add(node)
+            tree_nodes.update(traverse_marginal(node.child.down, interval))
+    elif type(node) is Coalescent:
+        if node.parent is None or interval_intersect([interval], node.parent.intervals):
+            tree_nodes.add(node)
+        del_child = None
+        for i, child in enumerate(node.children):
+            if interval_intersect([interval], child.intervals):
+                tree_nodes.update(traverse_marginal(child.down, interval))
+            else:
+                del_child = i
+        if del_child is not None:
+            del node.children[del_child]
+    return tree_nodes
 
-#     if type(child) is not Recombination:
-#          return True
-#     else:
-#         return child.left_parent == node and child.recomb_point == leftbreak \
-#            or child.right_parent == node and child.recomb_point == rightbreak
+def remove_dangling_root(tree_nodes):
+    """
+    Remove the nodes of a marginal tree or ARG 
+    from root to first coalescence
+    """    
+    for_del = list()
+    for i in range(len(tree_nodes)-1, -1, -1):
+        if type(tree_nodes[i]) is Coalescent and len(tree_nodes[i].children) == 2:
+            break
+        for_del.append(i)
+    for i in for_del:
+        del tree_nodes[i]
 
-# # what about break points and diamond recombinations?
+def marginal_arg(nodes, interval):
+    """
+    Gets the marginal ARG given a sequene interval
+    """    
+    # get nodes for marginal
+    marg_nodes = traverse_marginal(nodes[-1], list(interval))
 
-# def tree_nodes(node, visit):
+    # set to list
+    marg_nodes = list(marg_nodes)
 
-#     nodes = [node]
-#     if type(node) is Leaf:
-#         return nodes
-#     elif type(node) is Recombination:
-#         nodes[:0] = tree_nodes(node.child, visit)
-#         return nodes
-#     else:
-#         for child in node.children:  # could there be a sorting problem if coalescence children are not sorted?
-#             if visit(node, child):    # I guess not if we always use them in the same order.
-#                 nodes[:0] = tree_nodes(child, visit)
-#         return nodes
-#         # return node with children given by build_tree()
+    # sort on height
+    marg_nodes.sort(key=lambda x: x.height)
+    # prune top path above last coalescence
+    remove_dangling_root(marg_nodes)
 
-# def marginal_tree(node, leftbreak, rightbreak):
-    
-#     should_visit = partial(condition, 
-#         leftbreak=leftbreak, rightbreak=rightbreak)
+    return marg_nodes
 
-#     return tree_nodes(node, should_visit)
+def marginal_trees(nodes):
+    """
+    Gets list of marginal trees
+    """
+    tree_list = list()
+    breakpoints = get_breakpoints(nodes)
+    borders = [0] + breakpoints + [1]
+    for interval in zip(borders[:-1], borders[1:]):
+        marg_nodes = marginal_arg(nodes, interval)
 
-# def marginal_tree(nodes, interval):
-#     tree_nodes = list()
-#     for node in nodes:
-#         if [interval] == interval_intersect([interval], node.intervals):
-#             tree_nodes.append(node)
-#     return tree_nodes
+        tree_list.append(marg_nodes)
+    return tree_list
 
+def draw_graph(nodes):
+    """
+    Draws graph using matplotlib
+    """    
+    # make a graph and positions list
+    positions = list()
+    arg = nx.Graph()
+    for node in nodes:
+        arg.add_node(node.nodeid)
+        positions.append((node.xpos, node.height))
+        if isinstance(node, Recombination):
+            arg.add_edge(node.child.down.nodeid, node.nodeid)
+        elif isinstance(node, Coalescent):
+            for child in node.children:
+                arg.add_edge(child.down.nodeid, node.nodeid)
 
+    positions = np.array(positions)
+    positions = dict(zip(arg.nodes(), positions))
+
+    #pos = nx.spring_layout(arg)
+    nx.draw(arg, positions, alpha=0.5, node_size=200, with_labels=True)
+    #with_labels=True, 
+    #connectionstyle='arc3, rad = 0.1', 
+    #arrowstyle='-')
+
+    # G = nx.DiGraph() #or G = nx.MultiDiGraph()
+    # G.add_node('A')
+    # G.add_node('B')
+    # G.add_edge('A', 'B', length = 2)
+    # G.add_edge('B', 'A', length = 3)
+    # pos = nx.spring_layout(G)
+    # nx.draw(G, pos, with_labels=True, connectionstyle='arc3, rad = 0.1')
+    # edge_labels=dict([((u,v,),d['length'])
+    #              for u,v,d in G.edges(data=True)])
+
+    plt.show()
 
 if __name__ == '__main__':
 
-        # produce nodes in arg
+    # get arg and add positions
     nodes = get_arg_nodes()
-    print(nodes)
-    add_node_x_positions(nodes)
 
-    positions = get_positions(nodes)
-
+    # get breakpoints
     breakpoints = get_breakpoints(nodes)
 
-    print(breakpoints)
+    # get marginal trees
+    trees = marginal_trees(nodes)
 
-    # print(marginal_tree(nodes, (breakpoints[1], breakpoints[2])))
-    # print([n.intervals for n in nodes])
-
-    if True:
-        ###############################################
-        # draw a networkx graph
-        import matplotlib.pyplot as plt
-
-        # make a graph and positions list
-        positions = list()
-        arg = nx.Graph()
-        for node in nodes:
-            arg.add_node(node.nodeid)
-            positions.append((node.xpos, node.height))
-            if isinstance(node, Recombination):
-                arg.add_edge(node.child.nodeid, node.nodeid, length=node.height-node.child.height)
-            elif isinstance(node, Coalescent):
-                for child in node.children:
-                    arg.add_edge(child.nodeid, node.nodeid, length=node.height-child.height)
-
-        positions = np.array(positions)
-
-        #positions = dict(zip(arg.nodes(), positions))
-
-        #pos = nx.spring_layout(arg)
-        nx.draw(arg, positions, alpha=0.5, node_size=20)
-        #with_labels=True, 
-        #connectionstyle='arc3, rad = 0.1', 
-        #arrowstyle='-')
-
-        # G = nx.DiGraph() #or G = nx.MultiDiGraph()
-        # G.add_node('A')
-        # G.add_node('B')
-        # G.add_edge('A', 'B', length = 2)
-        # G.add_edge('B', 'A', length = 3)
-        # pos = nx.spring_layout(G)
-        # nx.draw(G, pos, with_labels=True, connectionstyle='arc3, rad = 0.1')
-        # edge_labels=dict([((u,v,),d['length'])
-        #              for u,v,d in G.edges(data=True)])
-
-        plt.show()
-        ###############################################
+    # marginal arg for some consequtive intervals
+    marg_arg = marginal_arg(nodes, [0, breakpoints[1]])
 
 
+    # draw graphs for testing
+    draw_graph(nodes)
+    draw_graph(marg_arg)
 
-
-
+    draw_graph(nodes)
+    for tree in marginal_trees(nodes):
+        print([n.xpos for n in tree])
+        draw_graph(tree)
 
 
